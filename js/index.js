c5fe614ad274d98fe34efb310b93c80b1868f830
github username <github email address>
2018-08-22 22:30:26 -0400

    fixed merge conflict

--- js/index.js
+++ js/index.js
@@ -2,6 +2,25 @@ document.addEventListener('DOMContentLoaded', init)
 
 function init(){
 
+  context = document.getElementById('canvas').getContext("2d");
+
+  $('#canvas').mousedown(function(e){
+    var mouseX = e.pageX - this.offsetLeft;
+    var mouseY = e.pageY - this.offsetTop;
+
+    paint = true;
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+    redraw();
+  });
+
+  $('#canvas').mouseup(function(e){
+    paint = false;
+  });
+
+  $('#canvas').mouseleave(function(e){
+    paint = false;
+  });
+
   var clickX = new Array();
   var clickY = new Array();
   var clickDrag = new Array();
@@ -34,44 +53,7 @@ function init(){
     }
   }
 
-
-
-  var canvasDiv = document.getElementById('canvasDiv');
-  canvas = document.createElement('canvas');
-  canvas.setAttribute('width', canvas.width);
-  canvas.setAttribute('height', canvas.height);
-  canvas.setAttribute('id', 'canvas');
-  canvasDiv.appendChild(canvas);
-  if(typeof G_vmlCanvasManager != 'undefined') {
-  	canvas = G_vmlCanvasManager.initElement(canvas);
-  }
-  context = canvas.getContext("2d");
-
-  $('#canvas').mousedown(function(e){
-    var mouseX = e.pageX - this.offsetLeft;
-    var mouseY = e.pageY - this.offsetTop;
-
-     paint = true;
-    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-    redraw();
-  });
-
-  $('#canvas').mousemove(function(e){
-    if(paint){
-      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-      redraw();
-    }
-  });
-
-  $('#canvas').mouseup(function(e){
-    paint = false;
-  });
-
-  $('#canvas').mouseleave(function(e){
-    paint = false;
-  });
-
-  // fetchBooks()
+  fetchBooks()
 }
 
 // FETCH BOOKS
